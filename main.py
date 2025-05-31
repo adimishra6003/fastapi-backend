2027795551bb50c157fa42709d14968db32f3fd2
adimishra6003 <adityamishra120799@gmail.com>
2025-05-31 19:30:02 +0530

    Implement Post model and update API endpoints for post creation

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
 
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+
 @app.get('/')
 async def root():
-    return {"message": "Welcome to API!"}
+    return {"message": "Welcome to my API!"}
+
+
+@app.get('/posts')
+async def get_posts():
+    return {"posts": "This is supposed to be a list of posts."}
+
+
+@app.post('/createpost')
+async def create_post(post: Post):
+    # print(post)
+    return {"new post": f"Title: {post.title} Content: {post.content} Published: {post.published} Rating: {post.rating}"}
